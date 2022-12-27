f8be41e2d9deda85f26de5c268265d4fab929c88
dell <dell@192.168.1.4>
2022-12-27 20:24:17 +0200

    First commit

---
+++ FawrySytem2_FawrySytem2/src/main/java/com/fcai/se/FawrySytem2/FawrySytem2Application.java
@@ -0,0 +1,13 @@
+package com.fcai.se.FawrySytem2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FawrySytem2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FawrySytem2Application.class, args);
+	}
+
+}
---
+++ FawrySytem2_FawrySytem2/src/test/java/com/fcai/se/FawrySytem2/FawrySytem2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcai.se.FawrySytem2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FawrySytem2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
